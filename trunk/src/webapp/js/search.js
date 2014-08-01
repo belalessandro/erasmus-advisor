91dfa83f570913b46ec02d231239f7d72d7abe68
schiappadini <schiappadini@83b1d799-25ee-ea07-702a-1643e04dd27a>
2014-08-01 14:09:56 +0000

    sincronizzando parte 2

---
+++ trunk/src/webapp/js/search.js
@@ -0,0 +1,6 @@
+$(function() { $('.linkUni').balloon({ contents: 'Clicca per vedere i dettagli' }); });
+		$(function() { $('.linkExam').balloon({ contents: 'Clicca per vedere i dettagli' }); });
+		$(function() { $('#alloggi').balloon({ contents: 'Presenza di alloggi' }); });
+		$(function() { $('.rowUni').balloon({ position: "right", 
+		contents: '<img src="../img/balloon-sample-loading.gif" alt="loading..." width="25" height="25" />', 
+		url: '/index.html h1' }); });
