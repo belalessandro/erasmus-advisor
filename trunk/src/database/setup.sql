01f254113d883bfd6657ac9b4900d0a9315b6947
belalessandro@gmail.com <belalessandro@gmail.com@83b1d799-25ee-ea07-702a-1643e04dd27a>
2014-08-09 17:48:05 +0000

    update setup

--- trunk/src/database/setup.sql
+++ trunk/src/database/setup.sql
@@ -8,6 +8,7 @@ drop database "erasmusadvisor";
 create database "erasmusadvisor";
 \c erasmusadvisor;
 \ir 'create.sql';   
-\ir 'triggers.sql';           
+\ir 'triggers.sql'; 
+\ir 'indexes.sql';           
 \ir 'fill.sql';               
 
