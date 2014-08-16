ca22e7107bd02eb20553cbe878089167af9877cb
belalessandro <belalessandro@users.noreply.github.com>
2014-08-16 16:43:16 +0000

    -metodi get e set su AreaBean

--- src/java/it/unipd/dei/bding/erasmusadvisor/beans/AreaBean.java
+++ src/java/it/unipd/dei/bding/erasmusadvisor/beans/AreaBean.java
@@ -17,6 +17,24 @@ class AreaBean implements Serializable {
 	private String nome;
 
 	/**
+	 * Returns the field nome.
+	 *
+	 * @return the value of nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * Sets the field nome
+	 *
+	 * @param nome The value to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	/**
 	 * Empty constructor
 	 */
 	public AreaBean() {}
