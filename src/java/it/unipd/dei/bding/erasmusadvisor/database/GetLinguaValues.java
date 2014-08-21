8b8dd85dc4a06c36954d86f00e69517d0274de51
belalessandro <belalessandro@users.noreply.github.com>
2014-08-21 16:32:28 +0000

    -aggiornamento DataSource -> connection servlet luca

--- src/java/it/unipd/dei/bding/erasmusadvisor/database/GetLinguaValues.java
+++ src/java/it/unipd/dei/bding/erasmusadvisor/database/GetLinguaValues.java
@@ -2,6 +2,7 @@ package it.unipd.dei.bding.erasmusadvisor.database;
 
 import it.unipd.dei.bding.erasmusadvisor.beans.LinguaBean;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -19,17 +20,17 @@ import org.apache.commons.dbutils.handlers.BeanListHandler;
 
 public class GetLinguaValues 
 {
-	public static List<LinguaBean> getLinguaDomain(DataSource ds) throws SQLException
+	public static List<LinguaBean> getLinguaDomain(Connection conn) throws SQLException
 	{
 		final String statement = "SELECT * FROM Lingua ORDER BY nome ASC"; 
 		
-		QueryRunner run = new QueryRunner(ds);
+		QueryRunner run = new QueryRunner();
 		
 		List<LinguaBean> lingue = null;
 		
 		ResultSetHandler<List<LinguaBean>> h1 = new BeanListHandler<LinguaBean>(LinguaBean.class);
-		lingue = run.query(statement, h1);
-				
+		lingue = run.query(conn, statement, h1);
+		
 		return lingue;
 	}
 }
