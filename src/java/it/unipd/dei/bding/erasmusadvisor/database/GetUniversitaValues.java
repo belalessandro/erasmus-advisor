d606aa2d147ece6442027910c0c33a4a1a27c0f7
belalessandro <belalessandro@users.noreply.github.com>
2014-08-23 15:42:15 +0000

    -fix

--- src/java/it/unipd/dei/bding/erasmusadvisor/database/GetUniversitaValues.java
+++ src/java/it/unipd/dei/bding/erasmusadvisor/database/GetUniversitaValues.java
@@ -4,6 +4,7 @@ import it.unipd.dei.bding.erasmusadvisor.beans.AreaBean;
 import it.unipd.dei.bding.erasmusadvisor.beans.UniversitaBean;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -37,6 +38,27 @@ public class GetUniversitaValues
 		
 		ResultSetHandler<List<UniversitaBean>> h1 = new BeanListHandler<UniversitaBean>(UniversitaBean.class);
 		universitaNames = run.query(conn, statement, h1);
+				
+		return universitaNames;
+	}
+	
+	/**
+	 * Get the name domains of Universita starting with some letters
+	 * @param conn A connection to the database.
+	 * @return a list of university names
+	 * @throws SQLException If an error occurs running the SQL query.
+	 */
+	public static List<UniversitaBean> getAreaDomainStartingWith(Connection conn, String with) throws SQLException
+	{
+		final String statement = "SELECT Nome FROM Universita "
+				+ "WHERE lower(Nome) LIKE lower(?)"; 
+		
+		QueryRunner run = new QueryRunner();
+		
+		List<UniversitaBean> universitaNames = null;
+		
+		ResultSetHandler<List<UniversitaBean>> h1 = new BeanListHandler<UniversitaBean>(UniversitaBean.class);
+		universitaNames = run.query(conn, statement, h1, with + "%");
 				
 		return universitaNames;
 	}
