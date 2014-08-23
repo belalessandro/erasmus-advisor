3e6d138fd9e778382ad2bb43669e2ebb9d873439
belalessandro <belalessandro@users.noreply.github.com>
2014-08-23 15:24:34 +0000

    -test di autocompletamento per universita

---
+++ src/java/it/unipd/dei/bding/erasmusadvisor/database/GetUniversitaValues.java
@@ -0,0 +1,43 @@
+package it.unipd.dei.bding.erasmusadvisor.database;
+
+import it.unipd.dei.bding.erasmusadvisor.beans.AreaBean;
+import it.unipd.dei.bding.erasmusadvisor.beans.UniversitaBean;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+
+/**
+ * Get the values of the domain of Universita
+ * @author Ale
+ *
+ */
+
+public class GetUniversitaValues 
+{
+	/**
+	 * Get the name domains of Universita
+	 * @param conn A connection to the database.
+	 * @return a list of university names
+	 * @throws SQLException If an error occurs running the SQL query.
+	 */
+	public static List<UniversitaBean> getAreaDomain(Connection conn) throws SQLException
+	{
+		final String statement = "SELECT Nome FROM Universita"; 
+		
+		QueryRunner run = new QueryRunner();
+		
+		List<UniversitaBean> universitaNames = null;
+		
+		ResultSetHandler<List<UniversitaBean>> h1 = new BeanListHandler<UniversitaBean>(UniversitaBean.class);
+		universitaNames = run.query(conn, statement, h1);
+				
+		return universitaNames;
+	}
+}
--- src/java/it/unipd/dei/bding/erasmusadvisor/servlets/UniversityListServlet.java
+++ src/java/it/unipd/dei/bding/erasmusadvisor/servlets/UniversityListServlet.java
@@ -1,13 +1,26 @@
 package it.unipd.dei.bding.erasmusadvisor.servlets;
 
 import it.unipd.dei.bding.erasmusadvisor.beans.UniversitaBean;
+import it.unipd.dei.bding.erasmusadvisor.database.GetUniversitaValues;
+import it.unipd.dei.bding.erasmusadvisor.resources.University;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.dbutils.DbUtils;
 
 /**
  * Servlet for getting lists of university
@@ -58,6 +71,40 @@ public class UniversityListServlet extends AbstractDatabaseServlet {
 			/* Show results to the JSP page. */
 			getServletContext().getRequestDispatcher("/jsp/search_university.jsp").forward(
 					req, resp);
+		} else if (true || "XMLHttpRequest".equals(req.getHeader("X-Requested-With"))) {
+			// Handle Ajax response (e.g. return JSON data object).
+
+			/**
+			 * GET FROM THE DB LIST OF ALL THE UNIVERSITIES
+			 * TODO: ci vorrebbe una ricerca STARTING WITH... 
+			 * ad es.. Select ... WHERE nomeUniversita IS LIKE aaa*;
+			 * 
+			 */
+			
+			// database connection
+			Connection conn = null;
+			
+			// model
+			List<UniversitaBean> nameList = null;
+			
+			try {
+				conn = DS.getConnection();
+				nameList = GetUniversitaValues.getAreaDomain(conn);
+			} catch (SQLException e) {
+				// Do nothing..
+			} 
+			finally {
+				DbUtils.closeQuietly(conn);
+			}
+			
+			resp.setContentType("application/json");
+			if (nameList != null) {
+				/* NOT IMPLEMENTED YET */
+				JsonWriter jsonWriter = Json.createWriter(resp.getWriter());
+				jsonWriter.writeArray(convertToJson(nameList));
+				jsonWriter.close();
+			}
+
 		} else { /* NO OPERATION */
 			
 			/* Redirect to the Search JSP page */
@@ -65,5 +112,18 @@ public class UniversityListServlet extends AbstractDatabaseServlet {
 		}
 
 	}
-
+	
+	private JsonArray convertToJson(List<UniversitaBean> nameList) {
+		// eg. {"id":"Botaurus stellaris","label":"Great Bittern","value":"Great Bittern"}
+		JsonArrayBuilder jb = Json.createArrayBuilder();
+		
+		for (UniversitaBean uni : nameList) {
+			jb.add(Json.createObjectBuilder()
+				     .add("id", uni.getNome())
+				     .add("label", uni.getNome())
+				     .add("value", uni.getNome()));
+		}
+		
+		return jb.build();
+	}
 }
