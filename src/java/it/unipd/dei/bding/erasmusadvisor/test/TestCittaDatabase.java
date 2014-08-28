23eb31c5bb65cb839631c3dd99d857e134efc16b
belalessandro <belalessandro@users.noreply.github.com>
2014-08-28 14:20:25 +0000

    -search city by lingua

---
+++ src/java/it/unipd/dei/bding/erasmusadvisor/test/TestCittaDatabase.java
@@ -0,0 +1,84 @@
+package it.unipd.dei.bding.erasmusadvisor.test;
+
+import it.unipd.dei.bding.erasmusadvisor.beans.CittaBean;
+import it.unipd.dei.bding.erasmusadvisor.beans.DocumentazioneBean;
+import it.unipd.dei.bding.erasmusadvisor.beans.FlussoBean;
+import it.unipd.dei.bding.erasmusadvisor.beans.GestioneBean;
+import it.unipd.dei.bding.erasmusadvisor.beans.InsegnamentoBean;
+import it.unipd.dei.bding.erasmusadvisor.beans.LinguaBean;
+import it.unipd.dei.bding.erasmusadvisor.beans.LinguaCittaBean;
+import it.unipd.dei.bding.erasmusadvisor.beans.LinguaTesiBean;
+import it.unipd.dei.bding.erasmusadvisor.beans.OrigineBean;
+import it.unipd.dei.bding.erasmusadvisor.beans.ProfessoreBean;
+import it.unipd.dei.bding.erasmusadvisor.beans.SvolgimentoBean;
+import it.unipd.dei.bding.erasmusadvisor.beans.UniversitaBean;
+import it.unipd.dei.bding.erasmusadvisor.beans.ValutazioneUniversitaBean;
+import it.unipd.dei.bding.erasmusadvisor.database.CittaDatabase;
+import it.unipd.dei.bding.erasmusadvisor.database.InsegnamentoDatabase;
+import it.unipd.dei.bding.erasmusadvisor.resources.CitySearchModel;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.List;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.dbutils.DbUtils;
+import org.apache.tomcat.jdbc.pool.DataSource;
+
+public class TestCittaDatabase {
+
+
+	private static final String DRIVER = "org.postgresql.Driver";
+
+	private static final String DATABASE = "jdbc:postgresql://localhost/erasmusadvisor";
+
+	/**
+	 * The username for accessing the database
+	 */
+	private static final String USER = "EATeam";
+
+	/**
+	 * The password for accessing the database
+	 */
+	private static final String PASSWORD = "EATeam";
+
+	public static void main(String[] args) {
+
+		Connection con = null;
+
+		try {
+			Class.forName(DRIVER);
+
+			System.out.printf("Driver %s successfully registered.%n", DRIVER);
+		} catch (ClassNotFoundException e) {
+			System.out.printf("Driver %s not found. %s.%n", DRIVER, e.getMessage());
+			System.exit(-1);
+		}
+
+
+		CittaBean cittaBean = new CittaBean();
+		
+		
+		try {
+			con = DriverManager.getConnection(DATABASE, USER, PASSWORD); // UNICA CONNESSIONE
+			//con.setAutoCommit(false);
+			
+			List<CitySearchModel> results = CittaDatabase.filterCityBySiglaLingua(con, "eng");
+			
+			for (CitySearchModel result : results) {
+				System.out.print(result.getCitta().getNome() + " | langs:");
+				for (LinguaBean l : result.getListaLingue()) {
+					System.out.print(" " + l.getNome());
+				}
+				System.out.print("\n");
+			}
+		} catch (SQLException e) {
+			//DbUtils.rollbackAndCloseQuietly(con); // ROLLBACK
+			e.printStackTrace();
+		} finally {
+			DbUtils.closeQuietly(con);
+		}
+	}
+}
