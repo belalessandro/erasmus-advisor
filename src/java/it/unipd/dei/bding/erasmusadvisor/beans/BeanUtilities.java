2e7cfde751c1cc7a8f6321211a0209b0484619dd
chiariotti.federico <chiariotti.federico@gmail.com>
2014-08-30 15:27:41 +0000

    POM: inserita dipendenza OWASP Creati metodi sanitizing in BeanUtilities (mancano quelli per i singoli Bean)

--- src/java/it/unipd/dei/bding/erasmusadvisor/beans/BeanUtilities.java
+++ src/java/it/unipd/dei/bding/erasmusadvisor/beans/BeanUtilities.java
@@ -5,54 +5,74 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.beanutils.BeanUtils;
+import org.owasp.html.HtmlSanitizer;
+import org.owasp.html.HtmlSanitizer.Policy;
+import org.owasp.html.PolicyFactory;
+import org.owasp.html.Sanitizers;
 
-/** Some utilities to populate beans, usually based on
- *  incoming request parameters. Requires three packages
- *  from the Apache Commons library: beanutils, collections,
- *  and logging. To obtain these packages, see
- *  http://jakarta.apache.org/commons/. Also, the book's
- *  source code archive (see http://www.coreservlets.com/)
- *  contains links to all URLs mentioned in the book, including
- *  to the specific sections of the Jakarta Commons package.
- *  <P>
- *  Note that this class is in the coreservlets.beans package,
- *  so must be installed in .../coreservlets/beans/.
+import com.sun.mail.util.BEncoderStream;
+
+/**
+ * Some utilities to populate beans, usually based on incoming request
+ * parameters. Requires three packages from the Apache Commons library:
+ * beanutils, collections, and logging. To obtain these packages, see
+ * http://jakarta.apache.org/commons/. Also, the book's source code archive (see
+ * http://www.coreservlets.com/) contains links to all URLs mentioned in the
+ * book, including to the specific sections of the Jakarta Commons package.
+ * <P>
+ * Note that this class is in the coreservlets.beans package, so must be
+ * installed in .../coreservlets/beans/.
  */
 
 public class BeanUtilities {
-  /** Examines all of the request parameters to see if
-   *  any match a bean property (i.e., a setXxx method)
-   *  in the object. If so, the request parameter value
-   *  is passed to that method. If the method expects
-   *  an int, Integer, double, Double, or any of the other
-   *  primitive or wrapper types, parsing and conversion
-   *  is done automatically. If the request parameter value
-   *  is malformed (cannot be converted into the expected
-   *  type), numeric properties are assigned zero and boolean
-   *  properties are assigned false: no exception is thrown.
-   */
+	/**
+	 * Examines all of the request parameters to see if any match a bean
+	 * property (i.e., a setXxx method) in the object. If so, the request
+	 * parameter value is passed to that method. If the method expects an int,
+	 * Integer, double, Double, or any of the other primitive or wrapper types,
+	 * parsing and conversion is done automatically. If the request parameter
+	 * value is malformed (cannot be converted into the expected type), numeric
+	 * properties are assigned zero and boolean properties are assigned false:
+	 * no exception is thrown.
+	 */
 
-  public static void populateBean(Object formBean,
-                                  HttpServletRequest request) {
-    populateBean(formBean, request.getParameterMap());
-  }
+	public static void populateBean(Object formBean, HttpServletRequest request) {
+		populateBean(formBean, request.getParameterMap());
+	}
 
-  /** Populates a bean based on a Map: Map keys are the
-   *  bean property names; Map values are the bean property
-   *  values. Type conversion is performed automatically as
-   *  described above.
-   */
+	/**
+	 * Populates a bean based on a Map: Map keys are the bean property names;
+	 * Map values are the bean property values. Type conversion is performed
+	 * automatically as described above.
+	 */
 
-  public static void populateBean(Object bean,
-                                  Map propertyMap) {
-    try {
-      BeanUtils.populate(bean, propertyMap);
-    } catch(Exception e) {
-      // Empty catch. The two possible exceptions are
-      // java.lang.IllegalAccessException and
-      // java.lang.reflect.InvocationTargetException.
-      // In both cases, just skip the bean operation.
-    }
-  }
+	public static void populateBean(Object bean, Map propertyMap) {
+		try {
+			Map sanitized=BeanUtilities.sanitize(propertyMap);
+			BeanUtils.populate(bean, sanitized);
+		} catch (Exception e) {
+			// Empty catch. The two possible exceptions are
+			// java.lang.IllegalAccessException and
+			// java.lang.reflect.InvocationTargetException.
+			// In both cases, just skip the bean operation.
+		}
+	}
+	
+	public static Map sanitize(Map propertyMap){
+		PolicyFactory policy = Sanitizers.FORMATTING.and(Sanitizers.LINKS);
+		for (Object key : propertyMap.keySet()) {
+			Object valueObj = propertyMap.get(key);
+			if (valueObj instanceof String) {
+				String value = (String) valueObj;
+				value = policy.sanitize(value);
+				propertyMap.put(key, value);
+			}
+		}
+		return propertyMap;
+	}
+	
+	public static String sanitize(String html){
+		PolicyFactory policy = Sanitizers.FORMATTING.and(Sanitizers.LINKS);
+		return policy.sanitize(html);
+	}
 }
-
