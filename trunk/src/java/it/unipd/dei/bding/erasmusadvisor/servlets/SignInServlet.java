1a4dec14f352649d8129f83cb50c6d0b2918cda7
chiariotti.federico@gmail.com <chiariotti.federico@gmail.com@83b1d799-25ee-ea07-702a-1643e04dd27a>
2014-08-30 21:09:14 +0000

    Signin differenziato per studente e responsabile

--- trunk/src/java/it/unipd/dei/bding/erasmusadvisor/servlets/SignInServlet.java
+++ trunk/src/java/it/unipd/dei/bding/erasmusadvisor/servlets/SignInServlet.java
@@ -1,7 +1,9 @@
 package it.unipd.dei.bding.erasmusadvisor.servlets;
 
 import it.unipd.dei.bding.erasmusadvisor.beans.BeanUtilities;
+import it.unipd.dei.bding.erasmusadvisor.beans.ResponsabileFlussoBean;
 import it.unipd.dei.bding.erasmusadvisor.beans.StudenteBean;
+import it.unipd.dei.bding.erasmusadvisor.database.CreateResponsabileDatabase;
 import it.unipd.dei.bding.erasmusadvisor.database.CreateStudenteDatabase;
 import it.unipd.dei.bding.erasmusadvisor.resources.LoggedUser;
 import it.unipd.dei.bding.erasmusadvisor.resources.Message;
@@ -19,7 +21,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
 /**
  * Form processing for the Sign in
  * 
@@ -27,17 +28,17 @@ import javax.servlet.http.HttpSession;
  * 
  */
 public class SignInServlet extends AbstractDatabaseServlet {
-	
+
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException 
-	{
+			throws ServletException, IOException {
 		// luca: il get restituisce il form, il post processa il sign in
 		// non facciamo porcherie con doPost che chiam il doGet
-		request.getRequestDispatcher("/jsp/sign_in.jsp").forward(request, response);
-		
+		request.getRequestDispatcher("/jsp/sign_in.jsp").forward(request,
+				response);
+
 	}
-		
-	private String hashPassword(String password, String salt){
+
+	private String hashPassword(String password, String salt) {
 		MessageDigest digest;
 		try {
 			digest = MessageDigest.getInstance("SHA-256");
@@ -58,52 +59,83 @@ public class SignInServlet extends AbstractDatabaseServlet {
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-
-		// model
-		StudenteBean s  = null;
+		String username = (String) request.getAttribute("user");
 		Message m = null;
+		UserType auth = UserType.STUDENTE;
 
-		try{
-			
-			s = new StudenteBean();
-			// Sporcooooo.. da valutare se utilizzare gli stessi nomi per la conversione automatica o farlo a mano
-			BeanUtilities.populateBean(s, request); // popola automaticam. i campi i cui nomi coincidono con quelli dei beans
-			SecureRandom random = new SecureRandom();
-			s.setSalt(""+random.nextLong());
-			s.setPassword(hashPassword(s.getPassword(), s.getSalt()));
-			s.setNomeUtente(request.getParameter("user"));
-			
-			LoggedUser logged=new LoggedUser(UserType.STUDENTE, s.getNomeUtente());
-			HttpSession session=request.getSession();
+		try {
+
+			String type = (String) request.getAttribute("userType");
+			if (type.equals("Manager")) {
+				auth = UserType.RESPONSABILE;
+				ResponsabileFlussoBean s = new ResponsabileFlussoBean();
+				// Sporcooooo.. da valutare se utilizzare gli stessi nomi per la
+				// conversione automatica o farlo a mano
+				BeanUtilities.populateBean(s, request); // popola automaticam. i
+														// campi i cui nomi
+														// coincidono con quelli
+														// dei
+														// beans
+				SecureRandom random = new SecureRandom();
+				s.setSalt("" + random.nextLong());
+				s.setPassword(hashPassword(s.getPassword(), s.getSalt()));
+				s.setNomeUtente(request.getParameter("user"));
+				s.setAbilitato(false);
+				s.setAttivo(true);
+
+				new CreateResponsabileDatabase(DS.getConnection(), s)
+						.createManager();
+			} else {
+				StudenteBean s = new StudenteBean();
+				// Sporcooooo.. da valutare se utilizzare gli stessi nomi per la
+				// conversione automatica o farlo a mano
+				BeanUtilities.populateBean(s, request); // popola automaticam. i
+														// campi i cui nomi
+														// coincidono con quelli
+														// dei
+														// beans
+				SecureRandom random = new SecureRandom();
+				s.setSalt("" + random.nextLong());
+				s.setPassword(hashPassword(s.getPassword(), s.getSalt()));
+				s.setNomeUtente(request.getParameter("user"));
+
+				new CreateStudenteDatabase(DS.getConnection(), s)
+						.createStudente();
+			}
+
+			LoggedUser logged = new LoggedUser(auth, username);
+			HttpSession session = request.getSession();
 			session.setAttribute("loggedUser", logged);
-			
-			new CreateStudenteDatabase(DS.getConnection(), s).createStudente();
-			
+
 		} catch (NumberFormatException ex) {
-			m = new Message("Cannot create the student. Invalid input parameters.", 
+			m = new Message(
+					"Cannot create the user. Invalid input parameters.",
 					"E100", ex.getMessage());
 		} catch (SQLException ex) {
 			if (ex.getSQLState().equals("23505")) {
-				m = new Message("Cannot create the student account: name " + s.getNomeUtente() + " already exists.", 
-						"E300", ex.getMessage());
+				m = new Message("Cannot create the user account: name "
+						+ username + " already exists.", "E300",
+						ex.getMessage());
 			} else {
-				m = new Message("Cannot create the student: unexpected error while accessing the database.", 
+				m = new Message(
+						"Cannot create the user: unexpected error while accessing the database.",
 						"E200", ex.getMessage());
 			}
 		}
-		
+
 		// stores the employee and the message as a request attribute
 		// request.setAttribute("student", s);
-		
+
 		if (m == null) {
 			// forwards the control to the index JSP
 			// luca: cambiato da user_profile a index.jsp
-			getServletContext().getRequestDispatcher("/jsp/index.jsp").forward(request, response);
-		} 
-		else {
+			getServletContext().getRequestDispatcher("/jsp/index.jsp").forward(
+					request, response);
+		} else {
 			// come back to sign_in JSP
 			request.setAttribute("message", m);
-			request.getRequestDispatcher("/jsp/sign_in.jsp").forward(request, response);
+			request.getRequestDispatcher("/jsp/sign_in.jsp").forward(request,
+					response);
 		}
 	}
 }
