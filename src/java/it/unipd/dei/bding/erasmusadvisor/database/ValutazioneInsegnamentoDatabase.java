2157794f6975511067202a108b86d83c6e058688
nicola.tommasi.91 <nicola.tommasi.91@gmail.com>
2014-09-03 07:56:42 +0000

    Fix Javadoc.

--- src/java/it/unipd/dei/bding/erasmusadvisor/database/ValutazioneInsegnamentoDatabase.java
+++ src/java/it/unipd/dei/bding/erasmusadvisor/database/ValutazioneInsegnamentoDatabase.java
@@ -18,67 +18,67 @@ import org.apache.commons.dbutils.handlers.BeanListHandler;
  */
 public class ValutazioneInsegnamentoDatabase 
 {
-	/**
-	 * Creates a new ValutazioneInsegnamento
-	 * 
-	 * @param con A connection to the database.
-	 * @param val An evaluation.
-	 * @throws SQLException If something goes wrong.
-	 */
-	public static void creaValutazioneInsegnamento(Connection con, ValutazioneInsegnamentoBean val)
-		throws SQLException
-	{
-		StringBuilder builder = new StringBuilder()
-			.append("INSERT INTO ValutazioneInsegnamento (nomeutentestudente, idinsegnamento, qtainsegnamanto, ")
-			.append("interesse, difficolta, rispettodelleore, datainserimento, commento) ")
-			.append("VALUES (?, ?, ?, ?, ?, ?, default, ?);");
-		
-		ResultSetHandler<ValutazioneInsegnamentoBean> rsh = 
-				new BeanHandler<ValutazioneInsegnamentoBean>(ValutazioneInsegnamentoBean.class);
-		QueryRunner run = new QueryRunner();
-		
-		run.insert(con, builder.toString(), rsh, 
-				val.getNomeUtenteStudente(),
-				val.getIdInsegnamento(),
-				val.getQtaInsegnamanto(),
-				val.getInteresse(),
-				val.getDifficolta(),
-				val.getRispettoDelleOre(),
-				val.getCommento());
-	}
-	
-	/**
-	 * Returns all the evaluation to classes inserted by a specific student.
-	 * 
-	 * @param conn A connection to the database.
-	 * @param user The User.
-	 * @return A list of evaluations.
-	 * @throws SQLException If something goes wrong.
-	 */
-	public static List<ValutazioneInsegnamentoBean> getEvalByUser(Connection conn, String user) throws SQLException
-	{
-		final String statement = "SELECT * FROM ValutazioneInsegnamento WHERE nomeutentestudente = ?";
-		
-		QueryRunner run = new QueryRunner();
-		ResultSetHandler<List<ValutazioneInsegnamentoBean>> h = new BeanListHandler<ValutazioneInsegnamentoBean>(ValutazioneInsegnamentoBean.class);
-		
-		return run.query(conn, statement, h, user);
-	}
-	
-	/**
-	 * Delete an evaluation.
-	 * 
-	 * @param conn A connection to the database.
-	 * @param user The student that inserted the evaluation.
-	 * @param id The class evaluated.
-	 * @return The number of rows affected.
-	 * @throws SQLException If something goes wrong.
-	 */
-	public static int deleteEvaluation(Connection conn, String user, int id) throws SQLException
-	{
-		final String statement = "DELETE FROM ValutazioneInsegnamento WHERE nomeutentestudente = ? AND idinsegnamento = ?";
-		
-		QueryRunner run = new QueryRunner();
-		return run.update(conn, statement, user, id);
-	}
+        /**
+         * Creates a new ValutazioneInsegnamento.
+         * 
+         * @param con A connection to the database.
+         * @param val An evaluation.
+         * @throws SQLException If something goes wrong.
+         */
+        public static void creaValutazioneInsegnamento(Connection con, ValutazioneInsegnamentoBean val)
+                throws SQLException
+        {
+                StringBuilder builder = new StringBuilder()
+                        .append("INSERT INTO ValutazioneInsegnamento (nomeutentestudente, idinsegnamento, qtainsegnamanto, ")
+                        .append("interesse, difficolta, rispettodelleore, datainserimento, commento) ")
+                        .append("VALUES (?, ?, ?, ?, ?, ?, default, ?);");
+                
+                ResultSetHandler<ValutazioneInsegnamentoBean> rsh = 
+                                new BeanHandler<ValutazioneInsegnamentoBean>(ValutazioneInsegnamentoBean.class);
+                QueryRunner run = new QueryRunner();
+                
+                run.insert(con, builder.toString(), rsh, 
+                                val.getNomeUtenteStudente(),
+                                val.getIdInsegnamento(),
+                                val.getQtaInsegnamanto(),
+                                val.getInteresse(),
+                                val.getDifficolta(),
+                                val.getRispettoDelleOre(),
+                                val.getCommento());
+        }
+        
+        /**
+         * Returns all the evaluation to classes inserted by a specific student.
+         * 
+         * @param conn A connection to the database.
+         * @param user The User.
+         * @return A list of evaluations.
+         * @throws SQLException If something goes wrong.
+         */
+        public static List<ValutazioneInsegnamentoBean> getEvalByUser(Connection conn, String user) throws SQLException
+        {
+                final String statement = "SELECT * FROM ValutazioneInsegnamento WHERE nomeutentestudente = ?";
+                
+                QueryRunner run = new QueryRunner();
+                ResultSetHandler<List<ValutazioneInsegnamentoBean>> h = new BeanListHandler<ValutazioneInsegnamentoBean>(ValutazioneInsegnamentoBean.class);
+                
+                return run.query(conn, statement, h, user);
+        }
+        
+        /**
+         * Delete an evaluation.
+         * 
+         * @param conn A connection to the database.
+         * @param user The student that inserted the evaluation.
+         * @param id The class evaluated.
+         * @return The number of rows affected.
+         * @throws SQLException If something goes wrong.
+         */
+        public static int deleteEvaluation(Connection conn, String user, int id) throws SQLException
+        {
+                final String statement = "DELETE FROM ValutazioneInsegnamento WHERE nomeutentestudente = ? AND idinsegnamento = ?";
+                
+                QueryRunner run = new QueryRunner();
+                return run.update(conn, statement, user, id);
+        }
 }
