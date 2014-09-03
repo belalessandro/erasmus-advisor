e66ed79a28c50320e560a5072bc72324a521d735
belalessandro <belalessandro@users.noreply.github.com>
2014-09-03 14:28:19 +0000

    -fix

--- src/java/it/unipd/dei/bding/erasmusadvisor/servlets/InterestServlet.java
+++ src/java/it/unipd/dei/bding/erasmusadvisor/servlets/InterestServlet.java
@@ -39,6 +39,11 @@ import org.apache.commons.dbutils.DbUtils;
 
 public class InterestServlet extends AbstractDatabaseServlet 
 {
+	/**
+	 * Operation constants
+	 */
+	private static final String INSERT = "insert";
+    private static final String DELETE = "delete";
 
 	private static final long serialVersionUID = -177735757618533981L;
 	
@@ -58,8 +63,6 @@ public class InterestServlet extends AbstractDatabaseServlet
 			throws ServletException, IOException 
 	{		
 		
-		Connection conn = null;
-		Message m = null;
 		
 		// Gets operation parameter
 		String operation = req.getParameter("operation");
@@ -80,77 +83,126 @@ public class InterestServlet extends AbstractDatabaseServlet
 		/** 
 		 * OPERATION DISPATCHER 
 		 */
-		else if (operation.equals("delete"))
+		else if (operation.equals(DELETE))
 		{
-			String flow = req.getParameter("flowID");
-			long updatedInterests = 0;
-			
-			int results = 0;
-			try {
-				conn = DS.getConnection();
-				results = InteresseDatabase.removeInterest(conn, flow, lu.getUser());
-				updatedInterests = InteresseDatabase.getCountInteresseByFlusso(conn, flow);
-
-			} 
-			catch (SQLException ex) 
-			{
-				m = new Message("Error while deleting your interest.", "", ex.getMessage());
-			} 
-			finally {
-				DbUtils.closeQuietly(conn); // always closes the connection 
-			}
-			
-			if (m == null && results > 0)
-			{
-				resp.setContentType("text/plain"); 
-				resp.setCharacterEncoding("UTF-8");
-				resp.getWriter().write(updatedInterests + "");
-			}
-			else
-			{
-				req.setAttribute("message", m);
-				getServletContext().getRequestDispatcher("/jsp/error.jsp").forward(req, resp);
-			}
+			delete(req, resp, lu);
 		}
-		else if (operation.equals("insert"))
+		else if (operation.equals(INSERT))
 		{
-			String flow = req.getParameter("flowID");
-			long updatedInterests = 0;
-			
-			try {
-				conn = DS.getConnection();
-				InteresseDatabase.addInterest(conn, flow, lu.getUser());
-				updatedInterests = InteresseDatabase.getCountInteresseByFlusso(conn, flow);
-			} 
-			catch (SQLException ex) 
-			{
-				m = new Message("Error while inserting your interest.", "", ex.getMessage());
-			} 
-			finally {
-				DbUtils.closeQuietly(conn); // always closes the connection 
-			}
-			
-			if (m == null)
-			{	
-				/** 
-				 * Show results to the JSP page. 
-				 *
-				 */
-				resp.setContentType("text/plain"); 
-				resp.setCharacterEncoding("UTF-8");
-				resp.getWriter().write(updatedInterests + "");
-				//resp.setContentType("application/json");
-				//JsonWriter jsonWriter = Json.createWriter(resp.getWriter());
-				//jsonWriter.writeObject(Json.createObjectBuilder().add("updatedInterests", updatedInterests).build());
-				//jsonWriter.close();
-			}
-			else
-			{
-				req.setAttribute("message", m);
-				getServletContext().getRequestDispatcher("/jsp/error.jsp").forward(req, resp);
-			}
+			insert(req, resp, lu);
 		}
 	}
+	
+	/**
+	 * Handles logic for insert operation.
+	 * 
+	 * @param request 
+	 * 				request from the client
+	 * @param response 
+	 * 				response to the client 
+	 * @throws ServletException
+	 * 			 	if any error occurs while executing the servlet
+	 * @throws IOException
+	 *  			if any error occurs in the client/server communication.
+	 */
+	private void insert(HttpServletRequest req, HttpServletResponse resp, LoggedUser lu) 
+			throws ServletException, IOException  {
+		
+		// Gets parameters
+		String flow = req.getParameter("flowID");
+		
+		long updatedInterests = 0;
+		
+		// Connection and model
+		Connection conn = null;
+		Message m = null;
+		
+		try {
+			conn = DS.getConnection();
+			InteresseDatabase.addInterest(conn, flow, lu.getUser());
+			updatedInterests = InteresseDatabase.getCountInteresseByFlusso(conn, flow);
+		} 
+		catch (SQLException ex) 
+		{
+			m = new Message("Error while inserting your interest.", "", ex.getMessage());
+		} 
+		finally {
+			DbUtils.closeQuietly(conn); // always closes the connection 
+		}
+		
+		if (m == null)
+		{	
+			/** 
+			 * Show results to the JSP page. 
+			 *
+			 */
+			resp.setContentType("text/plain"); 
+			resp.setCharacterEncoding("UTF-8");
+			resp.getWriter().write(updatedInterests + "");
+			//resp.setContentType("application/json");
+			//JsonWriter jsonWriter = Json.createWriter(resp.getWriter());
+			//jsonWriter.writeObject(Json.createObjectBuilder().add("updatedInterests", updatedInterests).build());
+			//jsonWriter.close();
+		}
+		else
+		{
+			req.setAttribute("message", m);
+			getServletContext().getRequestDispatcher("/jsp/error.jsp").forward(req, resp);
+		}
+	}
+	
+	/**
+	 * Handles logic for delete operation.
+	 * 
+	 * @param request 
+	 * 				request from the client
+	 * @param response 
+	 * 				response to the client 
+	 * @throws ServletException
+	 * 			 	if any error occurs while executing the servlet
+	 * @throws IOException
+	 *  			if any error occurs in the client/server communication.
+	 */
+    private void delete(HttpServletRequest req, HttpServletResponse resp, LoggedUser lu) 
+    		throws ServletException, IOException 
+    {
+    	
+    	// Connection and model
+		Connection conn = null;
+		Message m = null;
+		
+		// Gets parameters
+    	String flow = req.getParameter("flowID");
+		long updatedInterests = 0;
+		
+		int results = 0;
+		try {
+			conn = DS.getConnection();
+			results = InteresseDatabase.removeInterest(conn, flow, lu.getUser());
+			updatedInterests = InteresseDatabase.getCountInteresseByFlusso(conn, flow);
+
+		} 
+		catch (SQLException ex) 
+		{
+			m = new Message("Error while deleting your interest.", "", ex.getMessage());
+		} 
+		finally {
+			DbUtils.closeQuietly(conn); // always closes the connection 
+		}
+		
+		if (m == null && results > 0)
+		{
+			resp.setContentType("text/plain"); 
+			resp.setCharacterEncoding("UTF-8");
+			resp.getWriter().write(updatedInterests + "");
+		}
+		else
+		{
+			req.setAttribute("message", m);
+			getServletContext().getRequestDispatcher("/jsp/error.jsp").forward(req, resp);
+		}
+    }
+	
 	
 	/**
      * Handles error forwarding between pages.
