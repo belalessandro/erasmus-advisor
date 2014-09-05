619a6f4c1fb0453143a9d07b4b0f46c0d336c42f
mr.charlie90@gmail.com <mr.charlie90@gmail.com@83b1d799-25ee-ea07-702a-1643e04dd27a>
2014-09-05 16:57:28 +0000

    Ristretto l'accesso di un flow manager ai flussi che gli appartengono.

--- trunk/src/java/it/unipd/dei/bding/erasmusadvisor/database/FlussoDatabase.java
+++ trunk/src/java/it/unipd/dei/bding/erasmusadvisor/database/FlussoDatabase.java
@@ -130,6 +130,64 @@ public class FlussoDatabase
 		return new Flow(flow, responsabile, corsiOrigine, certificati, listaValutazioni);
 	}
 	
+	
+	/**
+	 * Gets a flow, searched by ID and manager name, with the corresponding 
+	 * list of professors, courses, certificates, evaluations.
+	 * 
+	 * @param conn the connection.
+	 * @param ID The Flow ID.
+	 * @param managerName manager username.
+	 * @return A Flow with the corresponding list of professors, courses, certificates, evaluations.
+	 * @throws SQLException If an error occurs.
+	 */
+	public static Flow getFlusso(Connection conn, String ID, String managerName) throws SQLException 
+	{
+		final String statement1 = "SELECT * FROM Flusso WHERE ID = ? AND respflusso = ?";
+		final String statement2 = "SELECT * FROM ValutazioneFlusso WHERE IdFlusso = ?";
+		final String statement3 = "SELECT * FROM ResponsabileFlusso WHERE NomeUtente = ?";
+		final String statement4 = "SELECT C.Id, C.Nome, C.Livello, C.NomeUniversita FROM CorsoDiLaurea AS C "
+				+ "INNER JOIN Origine AS O ON C.Id = o.idcorso WHERE O.idflusso = ?";
+		final String statement5 = "SELECT C.nomelingua, C.livello FROM Certificatilinguistici AS C "
+				+ "INNER JOIN documentazione AS D ON C.nomelingua = d.nomecertificato AND c.livello = d.livellocertificato "
+				+ "WHERE d.idflusso = ?";
+		
+		FlussoBean flow = new FlussoBean();
+		ResponsabileFlussoBean responsabile = new ResponsabileFlussoBean();
+		List<CorsoDiLaureaBean> corsiOrigine = null;
+		List<CertificatiLinguisticiBean> certificati = null;
+		List<ValutazioneFlussoBean> listaValutazioni = null;
+
+		QueryRunner run = new QueryRunner();
+		
+		ResultSetHandler<FlussoBean> h1 = new BeanHandler<FlussoBean>(FlussoBean.class);
+		flow = run.query(conn, statement1, h1, ID, managerName);
+		
+		if (flow == null)
+		{
+			return null;
+		}
+
+		// Gets the evaluations
+		ResultSetHandler<List<ValutazioneFlussoBean>> h2 = new BeanListHandler<ValutazioneFlussoBean>(ValutazioneFlussoBean.class);
+		listaValutazioni = run.query(conn, statement2, h2, ID);
+		
+		// Gets the manager
+		ResultSetHandler<ResponsabileFlussoBean> h3 = new BeanHandler<ResponsabileFlussoBean>(ResponsabileFlussoBean.class);
+		responsabile = run.query(conn, statement3, h3, flow.getRespFlusso());
+		
+		// Gets the courses
+		ResultSetHandler<List<CorsoDiLaureaBean>> h4 = new BeanListHandler<CorsoDiLaureaBean>(CorsoDiLaureaBean.class);
+		corsiOrigine = run.query(conn, statement4, h4, ID);
+		
+		// Gets the certificates
+		ResultSetHandler<List<CertificatiLinguisticiBean>> h5 = new BeanListHandler<CertificatiLinguisticiBean>(CertificatiLinguisticiBean.class);
+		certificati = run.query(conn, statement5, h5, ID);
+		
+		// Returns the results
+		return new Flow(flow, responsabile, corsiOrigine, certificati, listaValutazioni);
+	}
+	
 	/**
 	 * Update the flow with the id given.
 	 * 
